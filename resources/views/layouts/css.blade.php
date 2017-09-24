4059877f90d07308e8ac729a6cdc4549d53e086d
Van Hieu <hieunv.coder@gmail.com>
2017-09-24 17:47:20 +0700

    Mix with css,js by npm

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -1,43 +1,4 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link href="/css/app.css" rel="stylesheet"/>
+<link href=" {{ mix('css/app.css') }}" rel="stylesheet"/>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"/>
 <link rel="stylesheet" type="text/css" href="/css/sweetalert.css">
-
-
-<style>
-body{
-    padding-top:65px;
-    background-color: #fff;
-}
-
-.circ{
-    width : 40px;
-    vertical-align: middle;
-    border-radius: 50%;
-    box-shadow: 0 0 0 2px #fff;
-    margin-top: 5px;
-}
-
-.circ:hover{
-    box-shadow: 0 0 0 3px #f00;
-}
-
-.p0{
-    padding: 0;
-}
-
-.pl0 {
-    padding-left: 0;
-}
-
-.pr0 {
-    padding-right: 0;
-}
-
-.mt_20 {
-   margin-top: 20px;
-}
-.carousel-inner img {
-    margin: auto;
-}
-</style>
--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,3 +1,8 @@
-<script src="/js/app.js"></script>
+<script src="{{ mix('js/app.js') }}"></script>
 <script src="/js/sweetalert.js"></script>
 
+<script>
+    window.Laravel = {!! json_encode([
+    'csrfToken' => csrf_token(),
+    ]) !!};
+</script>
